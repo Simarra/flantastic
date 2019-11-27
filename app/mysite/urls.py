cab4fb52e302c1fa38c5822c624522014170fcf8
loic.martel@outlook.com <loic.martel@outlook.com>
2019-11-27 14:35:00 +0100

    SSL & domaine name operational

--- app/mysite/urls.py
+++ app/mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flantastic/', include('flantastic.urls')),
+    path('', include('flantastic.urls')),
 ]
